837399528335dfd600e5ce59da6a68becbeb5c2f
Aditya Hajare <aditya43@gmail.com>
2020-10-02 08:01:50 +0530

    Added calculator module

---
+++ 01-Python/calculator.py
@@ -0,0 +1,15 @@
+def add(num1, num2):
+    """This function will add 2 numbers"""
+    return num1 + num2
+
+def sub(num1, num2):
+    """This function will subtract 2 numbers"""
+    return num1 - num2
+
+def mul(num1, num2):
+    """This function will multiply 2 numbers"""
+    return num1 * num2
+
+def div(num1, num2):
+    """This function will divide 2 numbers"""
+    return num1 / num2
